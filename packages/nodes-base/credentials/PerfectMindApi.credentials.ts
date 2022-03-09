c6634cecac2023dc745d5494e6577b4e4db6b3a0
Dipak Parmar <hi@dipak.tech>
2022-03-09 08:24:47 -0800

    add credentials for PerfectMind API

---
+++ packages/nodes-base/credentials/PerfectMindApi.credentials.ts
@@ -0,0 +1,43 @@
+import {
+  ICredentialType,
+  NodePropertyTypes,
+  INodeProperties,
+} from 'n8n-workflow';
+
+export class PerfectMindApi implements ICredentialType {
+  name = 'perfectMindApi';
+	icon = 'file:PerfectMind.png';
+	documentationUrl = 'perfectmind';
+  displayName = 'PerfectMind API';
+
+  properties: INodeProperties[] = [
+		{
+      displayName: 'API Key',
+      name: 'apiKey',
+      type: 'string' as NodePropertyTypes,
+      default: '',
+      required: true,
+    },
+    {
+      displayName: 'API Version',
+      name: 'apiVersion',
+      type: 'string' as NodePropertyTypes,
+      default: '2.0',
+			required: true,
+    },
+    {
+      displayName: 'Client Number',
+      name: 'clientNumber',
+      type: 'string' as NodePropertyTypes,
+      default: '',
+			required: true,
+    },
+    {
+      displayName: 'Sub Domain',
+      name: 'subDomain',
+      type: 'string' as NodePropertyTypes,
+      default: '',
+			required: true,
+    },
+  ];
+}
