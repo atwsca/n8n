74fa6f567d50def1322e77cea3c4d00ae9cc2475
Dipak Parmar <hi@dipak.tech>
2022-03-13 21:45:40 -0700

    🚨 lint-fix of perfectmind api credentials

--- packages/nodes-base/credentials/PerfectMindApi.credentials.ts
+++ packages/nodes-base/credentials/PerfectMindApi.credentials.ts
@@ -1,43 +1,43 @@
 import {
-  ICredentialType,
-  NodePropertyTypes,
-  INodeProperties,
+	ICredentialType,
+	INodeProperties,
+	NodePropertyTypes,
 } from 'n8n-workflow';
 
 export class PerfectMindApi implements ICredentialType {
-  name = 'perfectMindApi';
+	name = 'PerfectMindApi';
 	icon = 'file:PerfectMind.png';
 	documentationUrl = 'perfectmind';
-  displayName = 'PerfectMind API';
+	displayName = 'PerfectMind API';
 
-  properties: INodeProperties[] = [
+	properties: INodeProperties[] = [
 		{
-      displayName: 'API Key',
-      name: 'apiKey',
-      type: 'string' as NodePropertyTypes,
-      default: '',
-      required: true,
-    },
-    {
-      displayName: 'API Version',
-      name: 'apiVersion',
-      type: 'string' as NodePropertyTypes,
-      default: '2.0',
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string' as NodePropertyTypes,
+			default: '',
 			required: true,
-    },
-    {
-      displayName: 'Client Number',
-      name: 'clientNumber',
-      type: 'string' as NodePropertyTypes,
-      default: '',
+		},
+		{
+			displayName: 'API Version',
+			name: 'apiVersion',
+			type: 'string' as NodePropertyTypes,
+			default: '2.0',
 			required: true,
-    },
-    {
-      displayName: 'Sub Domain',
-      name: 'subDomain',
-      type: 'string' as NodePropertyTypes,
-      default: '',
+		},
+		{
+			displayName: 'Client Number',
+			name: 'clientNumber',
+			type: 'string' as NodePropertyTypes,
+			default: '',
 			required: true,
-    },
-  ];
+		},
+		{
+			displayName: 'Sub Domain',
+			name: 'subDomain',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+			required: true,
+		},
+	];
 }
